9744c9599f2df29946bf9a2127bf8aa71187e760
numan <muhammadnumanlatif@gmail.cm>
2021-11-17 12:48:39 +0500

    initial commit 17112021

---
+++ lib/widgets/tv.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app_flutter/utils/text.dart';
+
+class TV extends StatelessWidget {
+  final List tv;
+
+  const TV({Key key, this.tv}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          modified_text(
+            text: 'Top Rated Movies',
+            size: 26,
+          ),
+          SizedBox(height: 10),
+          Container(
+              height: 270,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: tv.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      width: 140,
+                      child: Column(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: NetworkImage(
+                                    'https://image.tmdb.org/t/p/w500' +
+                                        tv[index]['poster_path']),
+                              ),
+                            ),
+                            height: 200,
+                          ),
+                          SizedBox(height: 5),
+                          Container(
+                            child: Expanded(
+                              child: modified_text(
+                                  size: 15,
+                                  text: tv[index]['title'] != null
+                                      ? tv[index]['title']
+                                      : 'Loading'),
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  }))
+        ],
+      ),
+    );
+  }
+}
